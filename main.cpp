94455549214a3910b4ba9bb66859d7de018538e5
eriks <eriks.ercums@gmail.com>
2025-05-22 01:34:27 +0300

    Izveidota metode redektora skats

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include "PaligFunkcijas.h"
 #include "LietotajuParvaldnieks.h"
 #include "Lietotajs.h"
+#include "Redaktors.h"
+#include "Speletajs.h"
 
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
 
+void lietotajsVaiRedaktors();
+void redaktoraSkats();
+
 LietotajuParvaldnieks LP;
+Redaktors* redaktors = nullptr;
+Speletajs* speletajs = nullptr;
+
+int main(){
+    lietotajsVaiRedaktors();
+    
+    cout << "Programmas beigas!\n";
+}
 
 void lietotajsVaiRedaktors(){
     system("cls");
-    cout << "Vai vēlaties PIESLĒGTIES(P) vai REĢISTRĒTIES(R)?\n";
     string izvele;
 
     while(true){
         //Pārliecinās, ka izvele nav tukša
-        izvele = stringIevade(izvele);
+        izvele = stringIevade("Vai vēlaties PIESLĒGTIES(P) vai REĢISTRĒTIES(R)?\n");
         //Pārveidot izvele uz upper case
         uzUpperCase(izvele);
 
@@ -31,21 +43,31 @@ void lietotajsVaiRedaktors(){
         }
 
         system("cls");
-        cout << "Vai vēlaties PIESLĒGTIES(P) vai REĢISTRĒTIES(R)?\n";
     }
     system("cls");
 
     if(izvele == "P"){
         Lietotajs* lietotajs = LP.pieslegties();
-        if(lietotajs != nullptr){
-            lietotajs->printInfo();
-        }
-        else{
+        if(lietotajs == nullptr){
             //TODO:Izdomāt, ko darīt, ka tiek atriezts nullptr, pagaidām vēlreiz lietotajsVaiRedaktors();
             cout << "Litotājvārds vai parole nav pareiza!\nNospiediet ENTER\n";
             cin.get();
             lietotajsVaiRedaktors();
         }
+        if(lietotajs->getLoma() == "Redaktors"){
+            redaktors = new Redaktors(
+                lietotajs->getId(),
+                lietotajs->getLietotajvards(),
+                lietotajs->getParole()
+            );
+            //Atīram atmiņu, lietotajs vairs nebūs vajadzīgs, jo pie tā varēs nokļūt caur Redaktors objektu
+            delete lietotajs;
+            redaktoraSkats();
+        }
+        else{
+            //TODO:Iezveidot spēlētāja skata punktu
+        }
+
     }
     else if(izvele == "R"){
         LP.izveidotProfilu();
@@ -54,8 +76,39 @@ void lietotajsVaiRedaktors(){
     }
 }
 
-int main(){
-    lietotajsVaiRedaktors();
-    
-    cout << "Programmas beigas!\n";
+void redaktoraSkats(){
+    if(redaktors == nullptr) return;
+
+    string ievade = "";
+    while(ievade != "STOP" && ievade != "EXIT"){
+        system("cls");
+        cout << "\tREDAKTORA SKATS\n";
+        //TODO:Pievienot vēl iespējas redaktoram
+        cout << "1) Izveidot jaunu spēli - nav izveidota;\n";
+        cout << "2) Apskatīt izveidotās spēles;\n";
+        cout << "3) Rediģēt spēles jautājumus; \n";
+        cout << "4) Dzēst spēles jautājumus; \n";
+        cout << "STOP vai EXIT - beigt darbu;\n\n";
+        ievade = stringIevade("Izvēle: ");
+        uzUpperCase(ievade);
+
+        system("cls");
+        cout << "\tREDAKTORA SKATS\n";
+        if(ievade == "1"){
+
+        }
+        else if (ievade == "2"){
+            redaktors->apskatitIzveidotasSpeles();
+        }
+        else if(ievade == "3"){
+            redaktors->redigetJautajumu();
+        }
+        else if(ievade == "4"){
+            redaktors->dzestJautajumu();
+        }
+        cout << "Nospiediet - ENTER";
+        cin.get();
+    }
+    delete redaktors;
+    redaktors = nullptr;
 }
